75bf54d94269e4c44827b6948bd963da00ae3b16
İnci Adıyaman <inci.adiyaman@hotmail.com>
2022-08-09 17:14:09 +0300

    test added for product controller

---
+++ StockTracking.Tests/ControllerTests/ProductControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using StockTracking.API.Controllers;
+using StockTracking.Business.Abstract;
+using StockTracking.Model.Dtos.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StockTracking.Tests.ControllerTests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void ProductController_Get_shouldReturnOk()
+        {
+            var productServiceMock = new Mock<IProductService>();
+
+            productServiceMock.Setup(x => x.GetAllProducts())
+                              .Returns(new List<GetAllProductsDto>()
+                              {
+                                  new GetAllProductsDto()
+                                  {
+                                      Name = "test",
+                                      Quantity = 1
+                                  }
+                              });
+
+            var productController = CreateInstance(productServiceMock);
+
+            var result = productController.Get();
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        private ProductController CreateInstance(Mock<IProductService> productServiceMock)
+        {
+            var productController = new ProductController(productServiceMock.Object)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = new DefaultHttpContext()
+                }
+            };
+
+            return productController;
+        }
+    }
+}
