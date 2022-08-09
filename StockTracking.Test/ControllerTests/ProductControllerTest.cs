080e0b349b417c97c7a732d743685c08c70effdd
İnci Adıyaman <inci.adiyaman@hotmail.com>
2022-08-09 16:48:24 +0300

    productcontroller test added

---
+++ StockTracking.Test/ControllerTests/ProductControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using StockTracking.API.Controllers;
+using StockTracking.Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StockTracking.Test.ControllerTests
+{
+    public class ProductControllerTest
+    {
+        private ProductController CreateInstance(Mock<IProductService> productServiceMock)
+        {
+            var productController = new ProductController(productServiceMock.Object)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = new DefaultHttpContext()
+                }
+            };
+
+            return productController;
+        }
+    }
+}
